b1b8b77ec328e3e41d6e364811f1bc6bf5acba3c
Anthony Guekdjian <anthonyguekdjian@gmail.com>
2023-10-31 19:39:38 -0300

    harcodeando datos sensibles.

--- backend-generic/app.js
+++ backend-generic/app.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
-const port = process.env.PORT;
+const port = 8080;
 
 connection();
 
--- backend-generic/src/services/jwt.js
+++ backend-generic/src/services/jwt.js
@@ -1,26 +1,26 @@
-const jwt = require("jwt-simple")
-const moment = require("moment")
-require("dotenv").config()
+const jwt = require("jwt-simple");
+const moment = require("moment");
+require("dotenv").config();
 
-const secret = process.env.JWT_SECRET
+const secret = "b5a1453zxk";
 
 const createToken = (user) => {
-    const payload = {
-        id: user._id,
-        name: user.name,
-        surname: user.surname,
-        email: user.email,
-        role: user.role,
-        image: user.image,
-        created_at: user.created_at,
-        iat: moment().unix(),
-        exp: moment().add(5, "days").unix()
-    }
+  const payload = {
+    id: user._id,
+    name: user.name,
+    surname: user.surname,
+    email: user.email,
+    role: user.role,
+    image: user.image,
+    created_at: user.created_at,
+    iat: moment().unix(),
+    exp: moment().add(5, "days").unix(),
+  };
 
-    return jwt.encode(payload, secret)
-}
+  return jwt.encode(payload, secret);
+};
 
 module.exports = {
-    secret,
-    createToken
-}
+  secret,
+  createToken,
+};
