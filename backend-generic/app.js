b4909c6e31437e8ec3d413659df7984918219988
Anthony Guekdjian <anthonyguekdjian@gmail.com>
2023-10-31 19:24:51 -0300

    Agregando ci/cd v2

--- backend-generic/app.js
+++ backend-generic/app.js
@@ -1,32 +1,30 @@
-const connection = require('./src/db/connection')
-const express = require('express')
-const cors = require('cors')
-require('dotenv').config()
+const connection = require("./src/db/connection");
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
 
-const app = express()
-const port = process.env.PORT
+const app = express();
+const port = process.env.PORT;
 
-connection()
+connection();
 
-app.use(cors())
+app.use(cors());
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // rutas
-const UserRoutes = require("./src/routes/user")
+const UserRoutes = require("./src/routes/user");
 
-app.use("/api/user", UserRoutes)
+app.use("/api/user", UserRoutes);
 
 // Ruta de prueba
-app.get('/', (req, res) => {
-    return res.status(200).json(
-        {
-            "message": "Aplicacion iniciada!!!"
-        }
-    )
-})
+app.get("/", (req, res) => {
+  return res.status(200).json({
+    message: "Aplicacion iniciada!!",
+  });
+});
 
 app.listen(port, () => {
-    console.log("Servidor iniciado en el puerto: ", port)
-})
+  console.log("Servidor iniciado en el puerto: ", port);
+});
